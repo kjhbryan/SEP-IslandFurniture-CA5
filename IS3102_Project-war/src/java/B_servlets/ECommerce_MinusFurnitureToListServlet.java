09c34e3ef8ee849e7667a6952245a5eda1bca1cb
Chua Meiling <chuameiling8@gmail.com>
2018-02-03 17:48:59 +0800

    Added ECommerce_MinusFurnitureToList servlet to remove a single quantity from the cart and show validation where the item cannot be less than 1

---
+++ IS3102_Project-war/src/java/B_servlets/ECommerce_MinusFurnitureToListServlet.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package B_servlets;
+
+import HelperClasses.ShoppingCartLineItem;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Bryan K
+ */
+@WebServlet(name = "ECommerce_MinusFurnitureToListServlet", urlPatterns = {"/ECommerce_MinusFurnitureToListServlet"})
+public class ECommerce_MinusFurnitureToListServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    ArrayList<ShoppingCartLineItem> shoppingCart;
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            HttpSession session = request.getSession();
+            String SKU = request.getParameter("SKU");
+            shoppingCart = (ArrayList<ShoppingCartLineItem>) session.getAttribute("shoppingCart");
+            if (shoppingCart == null) {
+                shoppingCart = new ArrayList<>();
+            }
+            else
+            {
+                for(ShoppingCartLineItem shoppingCartLineItem : shoppingCart)
+                {
+                    if(shoppingCartLineItem.getSKU().equals(SKU))
+                    {
+                        if(shoppingCartLineItem.getQuantity() > 1)
+                        { 
+                            response.sendRedirect("/IS3102_Project-war/B/SG/shoppingCart.jsp?goodMsg=Item quantity reduced successfully!");
+                            shoppingCartLineItem.setQuantity(shoppingCartLineItem.getQuantity() - 1);
+                            session.setAttribute("shoppingCart", shoppingCart);
+                        }
+                        else
+                        {
+                            response.sendRedirect("/IS3102_Project-war/B/SG/shoppingCart.jsp?errMsg=Error. Quantity cannot be less than 1.");
+                            session.setAttribute("shoppingCart", shoppingCart);
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception ex) {
+            out.println("\n\n " + ex.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
